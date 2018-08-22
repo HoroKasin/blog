9cdeb39ba89aaa107611db6c4e22d58c043d4b0d
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-08-23 02:06:11 +0900

    Add files via upload

---
+++ Day1_HelloJAVA/src/CommentExample.java
@@ -0,0 +1,9 @@
+public class CommentExample {
+
+	public static void main(String[] args) {
+		// <-- Comment
+		
+		System.out.println("// Comment"); //Description
+	}
+		//very easy Comment 
+}
---
+++ Day1_HelloJAVA/src/SimpleOperation.java
@@ -0,0 +1,12 @@
+public class SimpleOperation {
+
+	public static void main(String[] args) {
+
+		System.out.println(1 + 2);
+		System.out.println(7 * 2);
+		System.out.println(10 / 2);
+		System.out.println(1 - 2);
+		System.out.println(4 % 3);
+	}
+
+}
