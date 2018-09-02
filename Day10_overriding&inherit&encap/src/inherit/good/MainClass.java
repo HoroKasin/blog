8e3c8c298d46695e6830a2e1f69d00db34998ccd
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-02 22:10:09 +0900

    Update MainClass.java

--- Day10_overriding&inherit&encap/src/inherit/good/MainClass.java
+++ Day10_overriding&inherit&encap/src/inherit/good/MainClass.java
@@ -5,25 +5,25 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		Person p = new Person();
-		p.name = "홍길동";
+		p.name = "Hong Gil Dong";
 		p.age = 34;
 		System.out.println(p.getInfo());
 		
 		Student s = new Student();
 		s.studentId = "20181112312";
-		s.name = "이순신";
+		s.name = "General Lee";
 		s.age = 19;
 		System.out.println(s.getInfo());
 		
 		Teacher t = new Teacher();
-		t.subject = "수학";
-		t.name = "신사임당";
+		t.subject = "mathematics";
+		t.name = "Shin Saimdang";
 		t.age = 43;
 		System.out.println(t.getInfo());
 		
 		Employee e = new Employee();
-		e.department = "영업부";
-		e.name = "장보고";
+		e.department = "Sales department";
+		e.name = "Jang Bogo";
 		e.age = 44;
 		System.out.println(e.getInfo());
 		
