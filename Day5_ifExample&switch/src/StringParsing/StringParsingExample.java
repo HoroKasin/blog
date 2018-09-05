bcc52c8de92698471a941ec0ddf2d3d8d93496e5
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:25:30 +0900

    Update StringParsingExample.java

--- Day5_ifExample&switch/src/StringParsing/StringParsingExample.java
+++ Day5_ifExample&switch/src/StringParsing/StringParsingExample.java
@@ -10,14 +10,14 @@ public class StringParsingExample {
 		System.out.println(s1 + s2);
 		
 		/*
-		 * 문자열 데이터를 기본데이터로 치환하려면
-		 * 파싱 메서드를 사용해야 합니다.
-		 * - String -> int로 변환
-		 * Integer.parseInt(문자열)
-		 * - String -> double로 변환
-		 * Double.parseDouble(문자열)
-		 * - 주의사항: 변환 전 문자열 내부데이터가 정수, 실수
-		 * 형태가 아닌 경우에는 에러가 발생함.
+		 * To replace string data with default data
+		 * Parsing method must be used
+		 * - String -> convert to int
+		 * Integer.parseInt
+		 * - String -> Convert to Double
+		 * Double.parseDouble
+		 * - Caution: internal data in string before conversion is integer, error
+		 * An error is generated if it is not in shape.
 		 */
 //		Integer.parseInt("hello");
 		
