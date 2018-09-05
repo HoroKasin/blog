7f4f60f3434ac6633ec123f3b82664ef8aa9d87c
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:06:09 +0900

    Update BitExample.java

--- Day4_Array&Example/src/Bit/BitExample.java
+++ Day4_Array&Example/src/Bit/BitExample.java
@@ -7,19 +7,19 @@ public class BitExample {
 		byte a = 5; //0000 0101
 		byte b = 3; //0000 0011
 		
-		//비트곱 0000 0001
+		//&  0000 0001
 		System.out.println(a & b);
 		System.out.println("=============");
 		
-		//비트합 0000 0111
+		//| 0000 0111
 		System.out.println(a | b);
 		System.out.println("=============");
 		
-		//배타적 논리합 0000 0110
+		//^ 0000 0110
 		System.out.println(a ^ b);
 		System.out.println("=============");
 		
-		//비트 이동 연산자(<< , >>)
+		//<< , >>
 		int i = 192;
 		System.out.println(i << 3);
 		System.out.println(i >> 2);
