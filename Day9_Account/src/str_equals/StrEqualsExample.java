6bb2d7f4ab1e5bf864f705366f9bf2953036e6de
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 03:16:38 +0900

    Update StrEqualsExample.java

--- Day9_Account/src/str_equals/StrEqualsExample.java
+++ Day9_Account/src/str_equals/StrEqualsExample.java
@@ -4,36 +4,36 @@ public class StrEqualsExample {
 
 	public static void main(String[] args) {
 		
-		String str1 = "루피";
-		String str2 = "루피";
+		String str1 = "Logic";
+		String str2 = "Logic";
 		
 		if(str1 == str2) {
-			System.out.println("str1과 str2는 주소값이 같다.");
+			System.out.println("Str1 and str2 have the same address value.");
 		}else {
-			System.out.println("str1과 str2는 주소값이 다르다.");
+			System.out.println("Str1 and str2 have different address values.");
 		}
 		
 		if(str1.equals(str2)) {
-			System.out.println("str1과 str2의 문자열이 같다.");
+			System.out.println("Str1 and str2 have the same address value.");
 		}else {
-			System.out.println("str1과 str2의 문자열이 다르다.");
+			System.out.println("Str1 and str2 have different address values.");
 		}
 		
-		String str3 = new String("쵸파");
-		String str4 = new String("쵸파");
+		String str3 = new String("Nijin");
+		String str4 = new String("Nijin");
 		
 		System.out.println(str3 == str4);
 		
 		if(str3 == str4) {
-			System.out.println("str3과 str4는 주소값이 같다.");
+			System.out.println("Str3 and str4 have the same address value.");
 		}else {
-			System.out.println("str3과 str4는 주소값이 다르다.");
+			System.out.println("Str3 and str4 have different address values.");
 		}
 		
 		if(str3.equals(str4)) {
-			System.out.println("str3과 str4는 문자열이 같다.");
+			System.out.println("Str3 and str4 have the same address value.");
 		}else {
-			System.out.println("str3과 str4는 문자열이 다르다.");
+			System.out.println("Str3 and str4 have different address values.");
 		}
 
 	}
