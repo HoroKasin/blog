840c7ab33a80fa07bb030dad75e9022a850bb92d
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:16:44 +0900

    Update ifExample1.java

--- Day5_ifExample&switch/src/IF/ifExample1.java
+++ Day5_ifExample&switch/src/IF/ifExample1.java
@@ -4,18 +4,18 @@ public class ifExample1 {
 
 	public static void main(String[] args) {
 
-		// 0 ~ 100까지의 정수형 난수를 발생시키기
+		// create an integer random number from 0 to 100
 		int point = (int)(Math.random() * 101);
-		System.out.println("점수:>" + point);
+		System.out.println("Score:>" + point);
 		
 		if(point >= 60) {
-			System.out.println("60점이상!!!");
-			System.out.println("합격입니다.");
+			System.out.println("More than 60 points!!!");
+			System.out.println("I'm accepted.");
 		}else {
-			System.out.println("60점이하ㅠㅠ");
-			System.out.println("불합격입니다.");
+			System.out.println("60 points or lower");
+			System.out.println("It's a failure.");
 		}
-		System.out.println("수고하셨습니다.!@");
+		System.out.println("Thank you for your efforts.!@");
 	}
 
 }
