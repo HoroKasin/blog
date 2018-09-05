fc4a6ab67d1126f9d74c6c3281b08b65f21158ce
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:08:37 +0900

    Update LogicalExample1.java

--- Day4_Array&Example/src/Logical/LogicalExample1.java
+++ Day4_Array&Example/src/Logical/LogicalExample1.java
@@ -4,11 +4,11 @@ public class LogicalExample1 {
 
 	public static void main(String[] args) {
 
-		//같은 타입의 변수를 선언할 때는 콤마(,)로 나열이 가능
+		// list variables of the same type comma(,)
 		int x = 10, y = 20;
 		
-		//if뒤에 조건식의 결과가 true면 if 블록 내부의 코드를 실행
-		//false면 else블록 내부의 코드를 실행
+		//perform code inside if conditional expression is true
+		//execute the code inside the else block on the false face
 		
 		if((x != 10) & (++y ==21)) {
 			System.out.println("true");
