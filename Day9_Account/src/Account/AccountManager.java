0f50930d4fd572f2fd0a51a07451b6f132f27be9
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 03:13:55 +0900

    Update AccountManager.java

--- Day9_Account/src/Account/AccountManager.java
+++ Day9_Account/src/Account/AccountManager.java
@@ -8,72 +8,72 @@ public class AccountManager {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("* °èÁÂ¸¦ µî·ÏÇÕ´Ï´Ù.");
+		System.out.println("* Register account number.");
 		
-		System.out.print("- ÀÌ¸§: ");
+		System.out.print("- name: ");
 		String name = scan.next();
 		
-		System.out.print("- ºñ¹Ğ¹øÈ£: ");
+		System.out.print("- pwd: ");
 		String pwd = scan.next();
 		
-		System.out.print("- ÀÔ±İ¾×: ");
+		System.out.print("- Input amount: ");
 		int bal = scan.nextInt();
 		
 		Account acc = new Account(name, pwd, bal);
 		
-		System.out.println(name + "´ÔÀÇ °èÁÂ°¡ °³¼³µÇ¾ú½À´Ï´Ù!");
+		System.out.println(name + "ë‹˜ì˜ Account opened!");
 		
 		while(true) {
 			
-			System.out.println("\n¸Ş´º¸¦ ÀÔ·ÂÇÏ¼¼¿ä.");
-			System.out.println("[ 1. ¿¹±İ | 2. Ãâ±İ | 3. ÀÜ¾×Á¶È¸ | 4. Á¾·á ]");
+			System.out.println("\nPlease enter a menu.");
+			System.out.println("[ 1. Deposit | 2. Withdrawal | 3. Balance check | 4. Termination ]");
 			System.out.print("> ");
 			int menu = scan.nextInt();
 			
 			if(menu == 1) {
-				System.out.println("°èÁÂ ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇÏ¼¼¿ä.");
+				System.out.println("Please enter your account password.");
 				System.out.print("> ");
 				String pw = scan.next();
 				
 				if(pw.equals(acc.pwd)) {
-					System.out.println("ÀÔ±İ¾×À» ÀÔ·ÂÇÏ¼¼¿ä.");
+					System.out.println("Enter deposit amount.");
 					System.out.print("> ");
 					int money = scan.nextInt();
 					
 					acc.saveMoney(money);
 					
-					System.out.println("[°èÁÂ¹øÈ£: " + acc.accNum + "]");
-					System.out.println(money + "¿ø ÀÔ±İ ¿Ï·á!");
+					System.out.println("[bank account number: " + acc.accNum + "]");
+					System.out.println(money + "Deposit completed!");
 				}else {
-					System.out.println("ºñ¹Ğ¹øÈ£°¡ Æ²·È½À´Ï´Ù.");
+					System.out.println("The password is incorrect..");
 				}				
 			}else if(menu == 2) {
-				System.out.println("°èÁÂ ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇÏ¼¼¿ä.");
+				System.out.println("Please enter your account password.");
 				System.out.print("> ");
 				String pw = scan.next();
 				
 				if(pw.equals(acc.pwd)) {
-					System.out.println("Ãâ±İ¾×À» ÀÔ·ÂÇÏ¼¼¿ä.");
+					System.out.println("Please enter the withdrawal amount.");
 					System.out.print("> ");
 					int money = scan.nextInt();
 					
-					//Ãâ±İ¾×ÀÌ ÀÜ¾×À» ÃÊ°úÇÏ¸é Ãâ±İ ºÒ°¡
+					//not drain if the amount exceeds the balance
 					if(money > acc.balance) {
-						System.out.println("ÀÜ¾×ÀÌ ºÎÁ·ÇÕ´Ï´Ù.");
+						System.out.println("Insufficient balance.");
 					}else {
 						acc.withDrawMoney(money);
-						System.out.println("[°èÁÂ¹øÈ£: " + acc.accNum + "]");
-						System.out.println(money + "¿ø Ãâ±İ ¿Ï·á!");
+						System.out.println("[bank account number: " + acc.accNum + "]");
+						System.out.println(money + "Finished withdrawals!");
 					}					
 				}else {
-					System.out.println("ºñ¹Ğ¹øÈ£°¡ Æ²·È½À´Ï´Ù.");
+					System.out.println("The password is incorrect.");
 				}
 				
 			}else if(menu == 3) {
-				System.out.println(acc.name + "´ÔÀÇ °èÁÂ ÀÜ¾×Àº " 
-							+ acc.getBalance() + "¿øÀÔ´Ï´Ù.");
+				System.out.println(acc.name + "the balance of an account " 
+							+ acc.getBalance() + "$");
 			}else if(menu == 4) {
-				System.out.println("ÇÁ·Î±×·¥À» Á¾·áÇÕ´Ï´Ù.");
+				System.out.println("Exit the program.");
 				break;
 			}
 		}//end while
