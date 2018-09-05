aa6e8056c336a67523b74bd71ae0c1ed5fc981f9
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 01:47:05 +0900

    Update Identifier.java

--- Day2_Method/src/Identifier/Identifier.java
+++ Day2_Method/src/Identifier/Identifier.java
@@ -4,23 +4,22 @@ public class Identifier {
 
 	public static void main(String[] args) {
 		
-		//자바는 식별자 이름을 지을 때 대/소문자를 구분합니다.
+		//The Java is case sensitive when naming identifiers.
 		int age = 27;
 		int Age = 28;
 		int aGE = 29;
 		System.out.println(age);
 		System.out.println(Age);
 		
-		//식별자이름을 숫자로 시작하면 안됩니다.
-//		int 4number = 4; (X)//컨트롤 + / 빠른주석처리
+		//Do not start distinguished names with numbers
 		int number4 = 4;
 		int num4ber = 4;
 		
-		//식별자이름에는 공백이 들어가선 안됩니다.
+		//The identifier must not contain spaces
 //		int phone number = 12345678; (X)
 		int phoneNumber = 12345678;
 		
-		//키워드는 식별자로 사용 불가능
+		//unable as identifier
 //		int if = 4;
 //		int while = 4;
 	}
