3c16a726972e7d673aa9bafe485e772bb3f9638e
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:41:59 +0900

    Update MultiForExample.java

--- Day6_While,for/src/For/Basic/MultiForExample.java
+++ Day6_While,for/src/For/Basic/MultiForExample.java
@@ -4,7 +4,6 @@ public class MultiForExample {
 
 	public static void main(String[] args) {
 
-		//구구단 전체 출력
 		for(int i = 1; i <= 9; i++) {
 			for(int j = 2; j <= 9; j++) {
 				System.out.print(j + " x " + i + " = " + i * j + "\t");
