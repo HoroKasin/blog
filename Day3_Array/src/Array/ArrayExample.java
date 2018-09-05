9cc329e062b59260572feb91a0a6027fe42a1792
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 01:53:46 +0900

    Update ArrayExample.java

--- Day3_Array/src/Array/ArrayExample.java
+++ Day3_Array/src/Array/ArrayExample.java
@@ -6,28 +6,28 @@ public class ArrayExample {
 
 	public static void main(String[] args) {
 		
-		// 1. 배열의 선언
+		// 1. Declaration of Array
 		char[] letters; 
 		
-		// 2. 배열의 생성
+		//create an array
 		letters = new char[4];
 		
-		// 3. 배열의 초기화
-		// 각각의 인덱스에 접근하여 데이터를 저장함.
+		// 3. Initialize Array
+		// access each index and store data.
 		letters[0] = 'A';
 		letters[1] = 'B';
 		letters[2] = 'C';
 		letters[3] = 68;
 		
-		// 4. 배열의 값 참조( 사용  )
-		System.out.println("배열의 3번째 데이터의 값:>" + letters[2] );
+		// 4. See values in array (using )
+		System.out.println("Value of the third data in the array:>" + letters[2] );
 
 		/*
-		 * 배열 내부에 들어있는 데이터들을 문자열 형태로 한눈에
-		 * 확인해보려면 출력문 println()의 매개값으로 
-		 * Arrays.toString(배열변수명)을 써주면 됩니다.
+		 * Data contained within an array is presented in a string format.
+		 * To check, use the output statement println(). 
+		 * Use Array.toString.
 		 */
-		System.out.println("내부의 데이터를 문자열 형태로 보기:>" + Arrays.toString(letters));
+		System.out.println("View internal data in string form:>" + Arrays.toString(letters));
 	}
 
 }
