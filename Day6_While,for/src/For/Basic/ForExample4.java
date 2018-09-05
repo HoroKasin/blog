9589481643958c3c27e1a0255b7820a704d8b2da
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:39:34 +0900

    Update ForExample4.java

--- Day6_While,for/src/For/Basic/ForExample4.java
+++ Day6_While,for/src/For/Basic/ForExample4.java
@@ -4,7 +4,7 @@ public class ForExample4 {
 
 	public static void main(String[] args) {
 
-		// 1 ~100까지의 정수 중 8의 배수이면서 동시에 16의 배수가 아닌 수를 모두 출력
+		// drain 8 of the integer from 1 to 100 and output all non-drain numbers of 16 at the same time
 		for(int i = 1; i <=100; i++) {
 			if(i % 8 == 0 & i % 16 != 0) {
 				System.out.print(i + " ");
