7fea75aa53a954aa7c754a47ba065da53818e63f
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:15:29 +0900

    Update UnaryExample.java

--- Day4_Array&Example/src/Unary/UnaryExample.java
+++ Day4_Array&Example/src/Unary/UnaryExample.java
@@ -7,17 +7,17 @@ public class UnaryExample {
 		int i = 1;
 		int j = i++;
 		
-		System.out.println("후위 연산자");
-		System.out.println("i의 값은:>" + i);
-		System.out.println("j의 값은:>" + j);
+		System.out.println("postfix operators");
+		System.out.println("The value of i is:>" + i);
+		System.out.println("The value of j is:>" + j);
 		
 		System.out.println("=================");
 		
 		int x = 1;
 		int y = ++x;
-		System.out.println("전위 연산자");
-		System.out.println("x의 값은:>" + x);
-		System.out.println("y의 값은:>" + y);
+		System.out.println("Potential operator");
+		System.out.println("The value of x is:>" + x);
+		System.out.println("The price of y is:>" + y);
 		
 	}
 
