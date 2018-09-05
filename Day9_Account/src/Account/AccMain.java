32b6636c0e513f94776185c3f28de44e05f12737
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 03:10:53 +0900

    Update AccMain.java

--- Day9_Account/src/Account/AccMain.java
+++ Day9_Account/src/Account/AccMain.java
@@ -4,13 +4,13 @@ public class AccMain {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account("È«±æµ¿", "1234", 10000);
+		Account acc = new Account("Hong Gil Dong", "1234", 10000);
 		
 		acc.saveMoney(700);
 		acc.withDrawMoney(1900);
 		int bal = acc.getBalance();
 		
-		System.out.println(acc.name + "´ÔÀÇ °èÁÂ ÀÜ¾×Àº: " + bal + "¿øÀÔ´Ï´Ù.");
+		System.out.println(acc.name + "the balance of an account: " + bal);
 	}
 }
 
