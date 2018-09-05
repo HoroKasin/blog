38e643855c38f81a400bf50d10fa0a3c7e600e51
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:39:19 +0900

    Update ForExample3.java

--- Day6_While,for/src/For/Basic/ForExample3.java
+++ Day6_While,for/src/For/Basic/ForExample3.java
@@ -4,7 +4,7 @@ public class ForExample3 {
 
 	public static void main(String[] args) {
 
-		// 1 ~ 100 까지의 정수 중 6의 배수를 모두 출력
+		// print all multiples of 6 of the integer from 1 to 100
 		for(int i = 1; i <= 100; i++) {
 			if(i % 6 == 0) {
 				System.out.print(i + " ");
