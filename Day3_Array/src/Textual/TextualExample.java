3cffb5821233a75155152d8407177b71eca7e2d2
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:01:11 +0900

    Update TextualExample.java

--- Day3_Array/src/Textual/TextualExample.java
+++ Day3_Array/src/Textual/TextualExample.java
@@ -4,22 +4,22 @@ public class TextualExample {
 
 	public static void main(String[] args) {
 	
-		//단일문자를 저장할 수 있는 char
+		//use characters to store single characters
 		char c1 = 'A';
-		char c2 = '\uAC00'; //한글 '가'에 해당하는 유니코드 
-		char c3 = 66; //'b'의 유니코드값 16진수를 10진수로 변환한 값
+		char c2 = '\uAC00'; 
+		char c3 = 66; 
 		
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(c3);
 		
-		////문자열을 저장할 수 있는 String
+		//String that can store text
 		String s1 = "my name is 000";
 		String s2 = "computer hanashi desu";
 		
 		System.out.println(s1 + s2);
 		
-		//문자와 다른 데이터 타입 + 연산
+		//operate data type different from text
 		System.out.println("-----------------");
 		System.out.println(100 + 200);
 		System.out.println(100 + "200");
