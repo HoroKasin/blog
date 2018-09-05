ef3ef48df615f1b0497aef2230a19062f09373ec
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 02:36:15 +0900

    Update WhileExample2.java

--- Day6_While,for/src/While_/Basic/WhileExample2.java
+++ Day6_While,for/src/While_/Basic/WhileExample2.java
@@ -6,24 +6,24 @@ public class WhileExample2 {
 
 	public static void main(String[] args) {
 
-		//정수 하나를 입력받아서 해당 정수가 소수인지를 여부를 판단하는 방법
+		//How to determine if one integer is a decimal
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("소수를 판별할 정수를 입력:>");
-		//변수 n은 소수를 판별할 정수값을 저장할 변수공간
+		System.out.print("Enter an integer to determine the decimal:>");
+		//variable n is the space for the variable to store the integer value to determine the decimal number
 		int n = scan.nextInt();
 		
-		//변수 i는 소수판별을 위해 입력한 정수를 나눠볼 피제수를 저장할 변수공간
+		//parameter i is the variable space where the number of integers entered for decimals is stored.
 		int i = 2;
 		
 		while(n % i != 0) {
-			i++;   //입력받은 정수가 i로 나눠 0이 아닐때 계속 반복
+			i++;   //Divide input integer divided by i and repeat continuously when 0
 		}
 		
 		if( n == i) { 
-			System.out.println(n + "은(는) 소수입니다.");
+			System.out.println(n + "decimal");
 		}else {
-			System.out.println(n + "은(는) 소수가 아닙니다.");
+			System.out.println(n + "It is not a decimal.");
 		}
 		
 		scan.close();
