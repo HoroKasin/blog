c0141c38dc063462d232ba75ddcc74bf0e114c54
HoroKasin <41158812+HoroKasin@users.noreply.github.com>
2018-09-06 03:21:04 +0900

    Update MainClass.java

--- Day10_overriding&inherit&encap/src/overriding/calc/MainClass.java
+++ Day10_overriding&inherit&encap/src/overriding/calc/MainClass.java
@@ -7,12 +7,14 @@ public class MainClass {
 		double r = 15;
 		
 		Calculator cal = new Calculator();
-		System.out.println("계산기로 원의 넓이 계산");
+		System.out.println("Calculate the width of a 
+				   + circle with a calculator");
 		System.out.println(cal.areaCircle(r));
 		System.out.println("----------------");
 		
 		Computer com = new Computer();
-		System.out.println("컴퓨터로 원의 넓이 계산");
+		System.out.println("Calculate the width of a 
+				   + circle by computer");
 		System.out.println(com.areaCircle(r));
 
 	}
